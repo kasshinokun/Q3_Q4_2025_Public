9a70729f154758e305ab048d257505669e9e0081
Gabriel Cassino <120288615+kasshinokun@users.noreply.github.com>
2025-12-10 05:09:49 +0900

    Modify get_qr_code to return base64 image
    Updated the QR code generation to return a base64 encoded image.

--- 7_Semestre/LP/TTP_TP/TTP_e_TP1/Flask/Final/core/generate_code.py
+++ 7_Semestre/LP/TTP_TP/TTP_e_TP1/Flask/Final/core/generate_code.py
@@ -1,12 +1,18 @@
 import qrcode
+import base64
 
 def get_qr_code(dados:str): # 1. Recebe Dados para o QR code
 
     # 2. Criação do objeto QR code
-    img_data = qrcode.make(dados)
+    img = qrcode.make(dados)
 
-    # 3. Retorna o QR code para o solicitante
-    return img_data
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+
+    # 3. Retorna o QR code para o solicitante em base64 
+    return f"data:image/png;base64,{img_str}"
+    # return img_data
 
 # se quiser rodar e testar localmente
 #image=get_qr_code("https://github.com/kasshinokun/Q3_Q4_2025_Public/tree/main/7_Semestre/LP/TTP_TP")
